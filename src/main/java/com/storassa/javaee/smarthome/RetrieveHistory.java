5c76572d30d7ce40f83b02dbaeeb0b49ab48327d
Sergio Torassa <sertorassa@hotmail.com>
2016-08-01 17:49:25 +0200

    added Gson and servlet RetrieveHistory.java

---
+++ src/main/java/com/storassa/javaee/smarthome/RetrieveHistory.java
@@ -0,0 +1,50 @@
+package com.storassa.javaee.smarthome;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class RetrieveHistory
+ */
+@WebServlet("/history")
+public class RetrieveHistory extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	@EJB
+	MeasureEJB measureEjb;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RetrieveHistory() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+	    response.setCharacterEncoding("UTF-8");
+	    response.getWriter().write(new Gson().toJson(""));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
