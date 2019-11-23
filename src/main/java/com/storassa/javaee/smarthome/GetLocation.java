fbfd4e15a28c3e60fe6473b32ee18d6c79015e9b
sthor69 <sertorassa@hotmail.com>
2019-11-23 01:28:13 +0100

    Initial webhook test

---
+++ src/main/java/com/storassa/javaee/smarthome/GetLocation.java
@@ -0,0 +1,47 @@
+package com.storassa.javaee.smarthome;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetLocation
+ */
+@WebServlet("/getlocation")
+public class GetLocation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetLocation() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String lat = this.getServletConfig().getServletContext().getAttribute("lat").toString();
+		String lng = this.getServletConfig().getServletContext().getAttribute("lng").toString();
+		
+		request.setAttribute("lat", lat);
+		request.setAttribute("lng", lng);
+		
+		request.getRequestDispatcher("/location.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/com/storassa/javaee/smarthome/TelegramMessage.java
@@ -0,0 +1,47 @@
+package com.storassa.javaee.smarthome;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+
+@WebServlet("/telegramMessage")
+
+public class TelegramMessage extends HttpServlet {
+	
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TelegramMessage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		StringBuilder jsonBuff = new StringBuilder();
+		String line = null;
+		try {
+		    BufferedReader reader = request.getReader();
+		    while ((line = reader.readLine()) != null)
+		        jsonBuff.append(line);
+		} catch (Exception e) { /*error*/ }
+		
+		System.out.println("Request JSON string :" + jsonBuff.toString());
+		
+		request.getRequestDispatcher("/pages/dashboard.jsp").forward(request, response);
+
+	}
+
+}
